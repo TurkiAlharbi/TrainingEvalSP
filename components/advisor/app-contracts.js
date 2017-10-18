f85a81112aff9501a24caa077d2b87eb63d7a856
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-10-18 20:08:12 +0300

    Advisor's Views

---
+++ components/advisor/app-contracts.js
@@ -0,0 +1,69 @@
+app_contracts_template = `
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th v-for="header in headers">{{ header }}</th>
+        </tr>
+    </thead>
+    <tbody>
+
+        <template v-for="(contract,index) in contracts">
+
+            <tr data-toggle="collapse" data-parent="#accordion" :href="contract.hash">
+                <td> {{ contract.name }} </td>
+                <td> {{ contract.type }} </td>
+                <td> {{ contract.company }} </td>
+            </tr>
+            
+            <tr>
+                <div v-if="contract.company" :id="contract.id" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        
+                        <p>Country: {{ contract.info.country }}</p>
+                        <p>City: {{ contract.info.city }}</p>
+                        <p>Location: {{ contract.info.location }}</p>
+                        <p>Supervisor: {{ contract.info.supervisor }}</p>
+                        <a :href="contract.info.cid"> More details</a>
+                    </div>
+                </div>
+            </tr>
+
+            <tr>
+            
+            </tr>
+            
+        </template>
+
+    </tbody>
+</table>
+
+
+`;
+
+headers = ["Student Name", "Type", "Company"];
+
+contracts = [
+    {
+        id: "1", hash: "#1", name: "Ibrahim Al-Beladi", type: "Summer", company: "Aramco",
+        info: { country: "Saudi Arabia", city: "Dhahran", location: "Expec II", supervisor: "Maha Dossary", cid: "1" }
+    },
+    {
+        id: "2", hash: "#2", name: "Ibrahim Al-Beladi", type: "COOP", company: "Aramco",
+        info: { country: "Saudi Arabia", city: "Dhahran" }
+    },
+    {
+        id: "3", hash: "#3", name: "Ibrahim Al-Beladi" ,type: "Summer"
+    },
+    {
+        id: "4", hash: "#4", name: "Ibrahim Al-Beladi" ,type: "COOP"
+    },
+    {
+        id: "5", hash: "#5", name: "Ibrahim Al-Beladi" ,type: "Summer"
+    },
+];
+
+app_contracts = {
+    template: app_contracts_template,
+};
+
+Vue.component('app-contracts-table', app_contracts);
---
+++ components/advisor/app-students-table.js
@@ -0,0 +1,29 @@
+app_students_table_template = `
+<table class="table table-bordered table-striped table-hover">
+    <thead>
+        <tr>
+            <th v-for="header in headers">{{ header }}</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="student in students">
+            <td> {{ student.name }} </td>
+            <td> {{ student.major }} </td>
+            <td> {{ student.company }} </td>
+            <td> {{ student.supervisor }} </td>
+        </tr>
+    </tbody>
+</table>
+`;
+
+headers = ["Name", "Major", "Company", "Supervisor"];
+
+students = [
+    { name: "Ibrahim Al-Beladi", major: "ICS", company: "Aramco", supervisor: "Maha Al-Dossary" },
+];
+
+app_students_table = {
+    template: app_students_table_template,
+};
+
+Vue.component('app-students-table', app_students_table);
