0470ce067051307e8b3d191688fc7b16dfc2e4a2
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-10-18 14:51:54 +0300

    Work in progress

--- components/dashboard/app-option.js
+++ components/dashboard/app-option.js
@@ -2,10 +2,17 @@ app_option_template = `
 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
     <div class="card">
         <div class="thumbnail">
-            <div class="caption" style="text-align:center">
-                <hr style="width:50%;margin:15px auto">
-                <a :href="link">{{name}}</a>
-                <hr style="width:50%;margin:15px auto">
+            <div class="caption" style="text-align:center"  :style="{ 'background-color'  : '#eee' }">
+                <div v-if="link!=''">
+                    <hr style="width:50%;margin:15px auto">
+                    <a :href="link" class="small">{{name}}</a>
+                    <hr style="width:50%;margin:15px auto">
+                </div>
+                <div v-else>
+                    <p>{{name}}</p>
+                    <hr style="width:50%;margin:15px auto">
+                    <p class="small">Coming soon!</p>
+                </div>
             </div>
         </div>
     </div>
