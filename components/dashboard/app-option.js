9d6034ccf9fb3f9754fcf437bb9c42263cc6357c
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-10-19 13:00:46 +0300

    Option's minor style improvement

--- components/dashboard/app-option.js
+++ components/dashboard/app-option.js
@@ -1,13 +1,15 @@
 app_option_template = `
 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
     <div class="card">
-        <div class="thumbnail">
-            <div v-if="link!=''" class="caption" style="text-align:center">
+        <div v-if="link!=''" class="thumbnail panel panel-primary">
+            <div class="caption" style="text-align:center">
                 <hr style="width:50%;margin:15px auto">
                 <a :href="link" class="small">{{name}}</a>
                 <hr style="width:50%;margin:15px auto">
             </div>
-            <div v-else class="caption" style="text-align:center;background-color:#eee;">
+        </div>
+        <div v-else class="thumbnail">
+            <div class="caption" style="text-align:center;background-color:#eee;">
                 <p>{{name}}</p>
                 <hr style="width:50%;margin:15px auto">
                 <p style="font-size:12px">Work in progress!</p>
