9f72c422126c55115da2a824aff28f0fb7ff5938
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-10-19 10:59:13 +0300

    Removed register

--- components/login/app-container.js
+++ components/login/app-container.js
@@ -2,26 +2,10 @@ app_container_template = `
 <div class="container">
 	<div class="row">
 		<div class="col-md-6 col-md-offset-3">
-			<div class="panel with-nav-tabs panel-info">
-
-			<div class="panel-heading">
-				<ul class="nav nav-tabs">
-					<li class="active">
-						<a href="#login" data-toggle="tab">Login</a>
-					</li>
-					<li>
-						<a href="#register" data-toggle="tab">Register</a>
-					</li>
-				</ul>
-			</div>
-
-			<div class="panel-body">
-				<div class="tab-content">
+			<div class="panel panel-success">
+				<div class="panel-body">
 					<app-login></app-login>
-					<app-register></app-register>
 				</div>
-			</div>
-
 			</div>
 		</div>
 	</div>
