c9bf7981c12b5639941c46427ecb0b00a722aee0
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-11-01 14:21:34 +0300

    moved login scripts

---
+++ login/config.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCmJMfWLzjXoX90m52dRAIbGdALJfdwJdk",
+    authDomain: "eval-me.firebaseapp.com",
+    databaseURL: "https://eval-me.firebaseio.com",
+    projectId: "eval-me",
+    storageBucket: "eval-me.appspot.com",
+    messagingSenderId: "98801669023"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
