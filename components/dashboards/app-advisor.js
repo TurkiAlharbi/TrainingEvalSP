8a8466da494f55c72ff889d17769204e76938108
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-11-05 21:29:38 +0300

    marked done

--- components/dashboards/app-advisor.js
+++ components/dashboards/app-advisor.js
@@ -1,12 +1,12 @@
 template = `
 <div>
     <app-dashboard-no-container title="Supervisors management">
-        <app-option name="Add a supervisor" link="./Advisor/addSupervisor.html"></app-option>
+        <app-option name="Add a supervisor" link="./Advisor/addSupervisor.html" done="true"></app-option>
     </app-dashboard-no-container>
 
     <app-dashboard-no-container title="Students management">
-        <app-option name="View students" link="./Advisor/viewStudents.html"></app-option>
-        <app-option name="View contracts" link="./Advisor/viewContracts.html"></app-option>
+        <app-option name="View students" link="./Advisor/viewStudents.html" done="true"></app-option>
+        <app-option name="View contracts" link="./Advisor/viewContracts.html" done="true"></app-option>
     </app-dashboard-no-container>
 
     <app-dashboard-no-container title="Other utilities">
