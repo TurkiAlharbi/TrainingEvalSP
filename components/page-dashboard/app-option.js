36dcb0c890b689bcba7ae80826ff81ac70b29cdf
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-11-05 14:10:43 +0300

    removed gray if link is not there + added red if not dont

--- components/page-dashboard/app-option.js
+++ components/page-dashboard/app-option.js
@@ -1,19 +1,18 @@
 app_option_template = `
 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
     <div class="card">
-        <div v-if="link!=''" class="thumbnail panel panel-primary">
+        <div class="thumbnail panel panel-primary">
             <div class="caption" style="text-align:center">
                 <hr style="width:50%;margin:15px auto">
-                <a v-if="!popup" :href="link" class="small">{{name}}</a>
-                <a v-if="popup" target="_blank" :href="link+'?popup'" class="small" >{{name}}</a>
+                <template v-if="!done">
+                    <a v-if="!popup" style="color:red" :href="link" class="small">{{name}}</a>
+                    <a v-if=" popup" style="color:red" target="_blank" :href="link+'?popup'" class="small" >{{name}}</a>
+                </template>
+                <template v-else>
+                    <a v-if="!popup" :href="link" class="small">{{name}}</a>
+                    <a v-if=" popup" target="_blank" :href="link+'?popup'" class="small" >{{name}}</a>
+                </template>
                 <hr style="width:50%;margin:15px auto">
-            </div>
-        </div>
-        <div v-else class="thumbnail">
-            <div class="caption" style="text-align:center;background-color:#eee;">
-                <p>{{name}}</p>
-                <hr style="width:50%;margin:15px auto">
-                <p style="font-size:12px">Work in progress!</p>
             </div>
         </div>
     </div>
@@ -24,8 +23,9 @@ app_option = {
     props: {
         name: String,
         link: String,
-        popup: Boolean
-    }
+        popup: Boolean,
+        done: Boolean
+    },
 };
 
 Vue.component('app-option', app_option);
