232efeee3c894ba89016c833bc1485d5af389df1
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-11-05 21:28:00 +0300

    make advisor's stuff work with coordinator

--- components/advisor/app-contracts.js
+++ components/advisor/app-contracts.js
@@ -49,53 +49,61 @@ var students = [];
 setTimeout(function () {
 
     // Gets the advisor identifier (email)
-    advisor = firebase.auth().currentUser.email.split(".").join(" ");
+    advisor = firebase.auth().currentUser.email.split(".").join(" ");//or coordinator
+    
+    firebase.database().ref("users/" + advisor).once('value', function (snapshot0) {
 
-    // Connects to the advisors students
-    firebase.database().ref("advisors/" + advisor + "/students").once('value', function (snapshot) {
-
-        // Clears the old list
-        while (students.length > 0)
-            students.pop();
-
-        // Gets the snapshot of the data (students of the advisors)
-        vals = snapshot.val();
-
-        // For each student in the new list
-        for (var stu in vals) {
-
-            // Connect to get the student's data
-            firebase.database().ref("students/" + stu).once('value', function (snapshot2) {
-
-                // Gets the snapshot of the data (current student's data)
-                studentVals = snapshot2.val();
-
-                var student = {};
-                for (var key in studentVals) {
-                    student[key] = studentVals[key];
-                }
-
-                // Trying to get the student's supervisor's data
-                try {
-                    firebase.database().ref("supervisors/" + student.supervisor.split(".").join(" ")).once('value', function (snapshot4) {
-                        supVals = snapshot4.val();
-                        student.supervisor = supVals.name;
-                    });
-                } catch (err) {
-                    console.log(err.name);
-                }
-
-                // Highlight not submitting the contract yet
-                if (student.name == undefined) {
-                    student.name = snapshot2.key;
-                    student.supervisor = "<No contract>";
-                }
-                // Add to the list of students
-                students.push(student);
-            });
+        path = "advisors/";
+        if (snapshot0.val().type == "Coordinator") {
+            path = "coordinators/";
         }
+
+        // Connects to the advisor's/coordinator's students
+        firebase.database().ref(path + advisor + "/students").once('value', function (snapshot) {
+
+            // Clears the old list
+            while (students.length > 0)
+                students.pop();
+
+            // Gets the snapshot of the data (students of the advisors)
+            vals = snapshot.val();
+
+            // For each student in the new list
+            for (var stu in vals) {
+
+                // Connect to get the student's data
+                firebase.database().ref("students/" + stu).once('value', function (snapshot2) {
+
+                    // Gets the snapshot of the data (current student's data)
+                    studentVals = snapshot2.val();
+
+                    var student = {};
+                    for (var key in studentVals) {
+                        student[key] = studentVals[key];
+                    }
+
+                    // Trying to get the student's supervisor's data
+                    try {
+                        firebase.database().ref("supervisors/" + student.supervisor.split(".").join(" ")).once('value', function (snapshot4) {
+                            supVals = snapshot4.val();
+                            student.supervisor = supVals.name;
+                        });
+                    } catch (err) {
+                        console.log(err.name);
+                    }
+
+                    // Highlight not submitting the contract yet
+                    if (student.name == undefined) {
+                        student.name = snapshot2.key;
+                        student.supervisor = "<No contract>";
+                    }
+                    // Add to the list of students
+                    students.push(student);
+                });
+            }
+        });
     });
-}, 1000);
+}, 1250);
 
 app_contracts_table = {
     template: app_contracts_table_template,
