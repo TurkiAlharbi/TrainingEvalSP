8732f25967d90eca0ce85a752123a376663d0c73
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-11-13 15:33:12 +0300

    merged view and removed

--- components/dashboards/app-coordinator.js
+++ components/dashboards/app-coordinator.js
@@ -1,16 +1,15 @@
 template = `
 <div>
     <app-dashboard-no-container title="Advisors management">
-        <app-option name="View advisors" link="./Coordinator/viewAdvisors.html" done="true"></app-option>
         <app-option name="Add an advisor" link="./Coordinator/addAdvisor.html" done="true"></app-option>
-        <app-option name="Remove an advisor" link="./Coordinator/removeAdvisors.html" done="true"></app-option>
+        <app-option name="View and remove advisors" link="./Coordinator/removeAdvisors.html" done="true"></app-option>
     </app-dashboard-no-container>
 
     <app-dashboard-no-container title="Students management">
-        <app-option name="View students" link="./Coordinator/viewStudents.html" done="true"></app-option>
         <app-option name="Add a student" link="./Coordinator/addStudent.html" done="true"></app-option>
-        <app-option name="Remove a student" link="./Coordinator/removeStudents.html" done="true"></app-option>
+        <app-option name="View and remove students" link="./Coordinator/removeStudents.html" done="true"></app-option>
         <app-option name="Assign students to advisors" link="./Coordinator/assign.html" done="true"></app-option>
+        <app-option name="View contracts" link="./Coordinator/viewContracts.html" done="true"></app-option>
     </app-dashboard-no-container>
 
     <app-dashboard-no-container title="Evaluations management">
