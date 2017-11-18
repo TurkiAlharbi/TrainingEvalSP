6f242edb255bf1cc6f0f4ceb72131f9604781bed
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-11-18 20:22:59 +0300

    footer's margin

--- components/vuetify-dashboard/app-footer.js
+++ components/vuetify-dashboard/app-footer.js
@@ -1,11 +1,14 @@
 app_footer_template = `
-<v-footer class="blue white--text pb-3 pt-2" fixed>
-    
-    <v-spacer></v-spacer>
-    <div>EvaluateMe © {{ new Date().getFullYear() }}</div>
-    <v-spacer></v-spacer>
+<div>
+    <div class="pt-4 pb-5"></div>
+    <v-footer class="blue white--text pb-3 pt-2 mt-5" dense fixed>
+        
+        <v-spacer></v-spacer>
+        <div>EvaluateMe © {{ new Date().getFullYear() }}</div>
+        <v-spacer></v-spacer>
 
-</v-footer>
+    </v-footer>
+</div>
 `;
 
 app_footer = {
