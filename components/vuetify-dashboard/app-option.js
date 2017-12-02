f2c3d805e077fa7f6ebcff3cd2ca52314d7d8590
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-12-02 11:08:52 +0300

    cleaning

--- components/vuetify-dashboard/app-option.js
+++ components/vuetify-dashboard/app-option.js
@@ -1,17 +1,11 @@
 app_option_template = `
 <v-flex xs12 sm6 md4 lg3>
     <v-card class="ma-2">
-        <v-card-title class="justify-center" style="border:1px solid #2196f3" v-if="!wip">
+        <v-card-title class="justify-center" style="border:1px solid #2196f3">
             <div class="caption mb-2 mt-2">
                 <template>
                     <a :href="link" class="small" style="text-decoration: none">{{name}}</a>
                 </template>
-            </div>
-        </v-card-title>
-
-        <v-card-title v-else class="justify-center" style="border:1px solid #2196f3 ;background-color:#eee">
-            <div class="caption mb-2 mt-2">
-                <span>Coming up in next sprint(s)</span>
             </div>
         </v-card-title>
     </v-card>
@@ -22,9 +16,6 @@ app_option = {
     props: {
         name: String,
         link: String,
-        popup: Boolean,
-        done: Boolean,
-        wip: Boolean
     },
 };
 
