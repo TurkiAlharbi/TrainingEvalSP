d80799132f64aae840721f32e108327639479989
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-12-05 16:04:24 +0300

    expandable panels for the coordinator
    cuz he deserve it

--- components/dashboards/app-coordinator.js
+++ components/dashboards/app-coordinator.js
@@ -1,27 +1,27 @@
 template = `
 <div>
-    <app-dashboard-no-container title="Advisors management">
+    <app-expandable title="Advisors management" expanded>
         <app-option name="Add an advisor" link="./Coordinator/addAdvisor.html" done="true"></app-option>
         <app-option name="View and remove advisors" link="./Coordinator/removeAdvisors.html" done="true"></app-option>
         <app-option name="Assign students to advisors" link="./Coordinator/assign.html" done="true"></app-option>
-    </app-dashboard-no-container>
+    </app-expandable>
 
-    <app-dashboard-no-container title="Students management">
+    <app-expandable title="Students management" expanded>
         <app-option name="Add a student" link="./Coordinator/addStudent.html" done="true"></app-option>
         <app-option name="View and remove students" link="./Coordinator/removeStudents.html" done="true"></app-option>
         <app-option name="View contracts" link="./Coordinator/viewContracts.html" done="true"></app-option>
-    </app-dashboard-no-container>
+    </app-expandable>
 
-    <app-dashboard-no-container title="Evaluations management">
+    <app-expandable title="Evaluations management" expanded>
         <app-option name="View evaluations" link="./Coordinator/viewEvaluations.html" done="true"></app-option>
         <app-option name="Set up forms" link="./Coordinator/setup.html" done="true"></app-option>
         <app-option name="Set up evaluation availability " link="./Coordinator/evaluationAvailability.html" done="true"></app-option>
         <app-option name="Get students' marks" link="./Coordinator/getMarks.html" wip="true"></app-option>
-    </app-dashboard-no-container>
+    </app-expandable>
 
-    <app-dashboard-no-container title="Other utilities">
+    <app-expandable title="Other utilities" expanded>
         <app-option name="Generate a report" link="../Coordinator/generateReport.html" done="true"></app-option>
-    </app-dashboard-no-container>
+    </app-expandable>
     
     <!-- Advisor's view -->
     <app-expandable title="Advisor's roles">
