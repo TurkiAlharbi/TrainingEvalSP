5e7d9b48bcb321534f10ba301a693f68a81883ea
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-12-08 22:14:55 +0300

    minor thing -> large effect

--- components/vuetify-dashboard/app-option.js
+++ components/vuetify-dashboard/app-option.js
@@ -1,10 +1,10 @@
 app_option_template = `
 <v-flex xs12 sm6 md4 lg3>
-    <v-card class="ma-2">
+    <v-card class="ma-2" :href="link">
         <v-card-title class="justify-center" style="border:1px solid #2196f3">
             <div class="caption mb-2 mt-2">
                 <template>
-                    <a :href="link" class="small" style="text-decoration: none">{{name}}</a>
+                    <a class="small" style="text-decoration: none">{{name}}</a>
                 </template>
             </div>
         </v-card-title>
