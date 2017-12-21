7078d6c2dc362bdbfe12acbfd4dc29693407ce01
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-12-22 00:16:16 +0300

    verstile data export

--- res/js/export-table.js
+++ res/js/export-table.js
@@ -1,6 +1,16 @@
 function exportTable(filename) {
+    removeOtherExporters();
+    var settings = { formats: ['xls'], filename: filename, trimWhitespace: true };
+    var Table = document.getElementsByTagName("table");
     setTimeout(() => {
-        settings = { formats: ['xls'], filename: filename, trimWhitespace: true };
-        TableExport(document.getElementsByTagName("table"), settings);
+        exportInstance = TableExport(Table, settings);
     }, 500);
 }
+
+function removeOtherExporters() {
+    while (document.getElementsByClassName("button-default xls").length != 0) {
+        document.getElementsByClassName("button-default xls")[0].remove();
+    }
+}
+
+exportInstance = ''
